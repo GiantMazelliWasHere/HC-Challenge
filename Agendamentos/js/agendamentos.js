0c4bc7b3088e942f8d8b3c6b12c5fe5e661c1dcc
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2023-11-15 00:20:26 -0300

    mudanças no js

--- Agendamentos/js/agendamentos.js
+++ Agendamentos/js/agendamentos.js
@@ -3,13 +3,16 @@ const button = document.querySelector("#enviar");
 const inputNome = document.querySelector("#nome");
 const inputConsuta = document.querySelector("#consulta");
 const inputHorario = document.querySelector("#horario");
+const inputPesquisa = document.querySelector("#inputPesquisa")
+
+// Validação do Formulário
 
 // Elemento p para o erro
 const error = document.createElement('p');
 
 // Função de limpeza
 function clearForm() {
-    inputNome.value = '';
+    buttonNome.value = '';
     inputConsuta.value = '';
     inputHorario.value = '';
     error.textContent = '';
@@ -19,6 +22,9 @@ function clearForm() {
 button.addEventListener(("click"), function(event) {
     event.preventDefault();
 
+    // Validação
+
+    //Nome
     if (inputNome.value === '') {
         error.textContent = "Este campo é obrigatório";
         inputNome.insertAdjacentElement("afterend", error);
@@ -26,6 +32,7 @@ button.addEventListener(("click"), function(event) {
         return false;
     }
 
+    // Consulta
     if (inputConsuta.value === '') {
         error.textContent = "Este campo é obrigatório";
         inputConsuta.insertAdjacentElement("afterend", error);
@@ -33,6 +40,7 @@ button.addEventListener(("click"), function(event) {
         return false;
     }
 
+    // Horario
     if (inputHorario.value === '') {
         error.textContent = "Este campo é obrigatório";
         inputHorario.insertAdjacentElement("afterend", error);
@@ -40,9 +48,16 @@ button.addEventListener(("click"), function(event) {
         return false;
     }
 
-    console.log(inputNome.value);
-    console.log(inputConsuta.value);
-    console.log(inputHorario.value);
-    
     clearForm();
 })
+
+// Barra de Pesquisa
+
+function apareceBarraDePesquisa(){
+    inputPesquisa.style.display = 'inline-block';
+}
+
+function esconderBarraDePesquisa(){
+    inputPesquisa.style.display = 'none';
+}
+
