7fb63f4f8ddc186240a44a46607642512d106389
Eduardo Mazelli <eduardo.mazelli@gmail.com>
2023-11-15 14:51:14 -0300

    adicionand js paciente

---
+++ Paciente/js/paciente.js
@@ -0,0 +1,11 @@
+const inputPesquisa = document.querySelector("#inputPesquisa")
+
+// Barra de Pesquisa
+
+function apareceBarraDePesquisa(){
+    inputPesquisa.style.display = 'inline-block';
+}
+
+function esconderBarraDePesquisa(){
+    inputPesquisa.style.display = 'none';
+}
