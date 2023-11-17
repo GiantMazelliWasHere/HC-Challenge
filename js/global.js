944fbc694d566c52a9dd42d583745b6e5cbef585
CavMCarolina <RM552925@fiap.com.br>
2023-11-17 08:37:45 -0300

    Adicionando pasta js

---
+++ js/global.js
@@ -0,0 +1,68 @@
+const inputPesquisa = document.querySelector("#inputPesquisa")
+
+// Barra de Pesquisa
+
+function apareceBarraDePesquisa(){
+    inputPesquisa.style.display = 'inline-block';
+}
+
+function esconderBarraDePesquisa(){
+    inputPesquisa.style.display = 'none';
+}
+
+// Agendamentos:
+// Variavéis
+const button = document.querySelector("#enviar");
+const inputNome = document.querySelector("#nome");
+const inputConsulta = document.querySelector("#consulta");
+const inputHorario = document.querySelector("#horario");
+const inputPesquisa = document.querySelector("#inputPesquisa")
+
+// Validação do Formulário
+
+// Elemento p para o erro
+const error = document.createElement('p');
+
+// Função de limpeza
+function clearForm() {
+    inputNome.value = '';
+    inputConsulta.value = '';
+    inputHorario.value = '';
+    error.textContent = '';
+}
+
+// Criando o evento de botão
+button.addEventListener(("click"), function(event) {
+    event.preventDefault();
+
+    // Validação
+
+    //Nome
+    if (inputNome.value === '') {
+        error.textContent = "Este campo é obrigatório";
+        inputNome.insertAdjacentElement("afterend", error);
+        error.setAttribute("class", "error");
+        return false;
+    }
+
+    // Consulta
+    if (inputConsulta.value === '') {
+        error.textContent = "Este campo é obrigatório";
+        inputConsulta.insertAdjacentElement("afterend", error);
+        error.setAttribute("class", "error");
+        return false;
+    }
+
+    // Horario
+    if (inputHorario.value === '') {
+        error.textContent = "Este campo é obrigatório";
+        inputHorario.insertAdjacentElement("afterend", error);
+        error.setAttribute("class", "error");
+        return false;
+    }
+
+    console.log(inputNome.value)
+    console.log(inputConsulta.value)
+    console.log(inputHorario.value)
+    clearForm();
+})
